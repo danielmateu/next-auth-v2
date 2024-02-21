88326c9a459c9bf040bdd572cf125748bf5bdb4e
Daniel Mateu <danielmateu86@gmail.com>
2024-02-21 20:52:18 +0100

    register functionality updated

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server"
 
+import bcrypt from "bcrypt"
 import { RegisterSchema } from "@/schemas"
 import * as z from "zod"
+import { db } from "@/lib/db"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
@@ -10,7 +13,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: 'Campos invalidos' }
     }
 
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return { error: 'Este email ya existe' }
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            name
+        }
+    })
+
+    // Tdo: Send verification token email
+
     return {
-        success: "Email enviado"
+        success: "Usuario creado con exito!"
     }
 }

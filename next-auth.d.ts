fbf1bb1683f4fa09f9215b66cf9ef5f7f0ee5bd1
Daniel Mateu <danielmateu86@gmail.com>
2024-02-22 02:05:24 +0100

    callbacks updated

---
+++ next-auth.d.ts
@@ -0,0 +1,20 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & {
+    // id: string | undefined
+    role: "ADMIN" | "USER"
+}
+
+declare module "next-auth" {
+    interface Session {
+        user: ExtendedUser
+    }
+}
+
+import { JWT } from "@auth/core/jwt"
+
+declare module "@auth/core/jwt" {
+    interface JWT {
+        role?: "ADMIN" | "USER"
+    }
+}

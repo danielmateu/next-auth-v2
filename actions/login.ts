c56ec6c2f50ba94530d63e8fe7c79a5343f3c5dc
Daniel Mateu <danielmateu86@gmail.com>
2024-02-22 01:17:15 +0100

    middleware and login updated

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 "use server"
 
+import { signIn } from "@/auth"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 import { LoginSchema } from "@/schemas"
+import { AuthError } from "next-auth"
 import * as z from "zod"
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -10,7 +13,23 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         return { error: 'Campos invalidos' }
     }
 
-    return {
-        success: "Email enviado"
+    const { email, password } = validatedFields.data
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT
+        })
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Credenciais inválidas" }
+                default:
+                    return { error: "Algo ha salido mal" }
+            }
+        }
+        throw error
     }
 }
---
+++ routes.ts
@@ -0,0 +1,35 @@
+/*
+    * Un array de rutas que son accesibles sin autenticación
+    * Estas rutas son accesibles para todos los usuarios
+    * @type {string[]}
+*/
+
+export const publicRoutes = [
+    "/",
+]
+
+/*
+    * Un array de rutas que usaremos para la autenticación
+    * Estas rutas redirigiran los usuarios logueados a /settings
+    * @type {string[]}
+*/
+
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+]
+
+/*
+    * El prefijo de la API para las rutas de autenticación
+    * Las rutas que empiezan con este prefijo son usadas para la autenticación
+    * @type {string}
+*/
+
+export const apiAuthPrefix = "/api/auth"
+
+/*
+    * La ruta a la que redirigir si el usuario está autenticado
+    * @type {string}
+*/
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
